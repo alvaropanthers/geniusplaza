54cdbe9ff03316a6b2ff70e4179b429d096da593
Alvaro Orozco <aoroz032@fiu.edu>
2019-04-18 09:58:51 -0400

    Settings

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k6cebga9wlox8_w6l3@ftvldi1!c_qvfkfl35d0&odw#il--w2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver']
+ALLOWED_HOSTS = ['testserver', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'recipe',
 ]
 
